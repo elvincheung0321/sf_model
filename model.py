e381c1e9d16ffd0dbd36bed68c94e54666c646c4
Elvin Cheung <111124105+elvincheung0321@users.noreply.github.com>
2026-08-16 14:53:30 +0800

    Model finally works after training for around 10 hours, is able to get to the second level

--- model.py
+++ model.py
@@ -1,15 +1,13 @@
 import stable_retro as retro
-import gymnasium as gym
 from gymnasium import Env
 from gymnasium.spaces import MultiBinary, Box
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-import os
-import optuna
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import SubprocVecEnv, VecFrameStack
+from stable_baselines3.common.callbacks import CheckpointCallback
 from stable_baselines3 import PPO
 
 
@@ -38,8 +36,8 @@ class StreetFighter(Env):
             self.enemy_health = 0
             self.player_health = 0
         else:
-            dmg_dealt = self.enemy_health -info["enemy_health"]
-            dmg_taken = self.player_health- info["health"]
+            dmg_dealt = max(0, self.enemy_health -info["enemy_health"])
+            dmg_taken = max(0, self.player_health- info["health"])
             self.enemy_health = info["enemy_health"]
             self.player_health = info["health"]
             reward = dmg_dealt - dmg_taken
@@ -76,7 +74,14 @@ class StreetFighter(Env):
         self.game.close()
 
 LOG_DIR = "./opt_logs/"
-
+model_path = "./opt/trial_125_best_model.zip"
+save_path = "./opt/sf_5m_v2"
+callback = CheckpointCallback(
+    save_freq=100000,
+    save_path="./opt/checkpoints/",
+    name_prefix="sf_5m_v2"
+)
+timesteps = 5000000
 
 def make_env():
     return Monitor(StreetFighter(), LOG_DIR)
@@ -85,23 +90,24 @@ def make_env():
 def main():
     env = SubprocVecEnv([make_env for _ in range(4)])
     env = VecFrameStack(env, n_stack=4, channels_order="last")
-    model_params = {
-        "n_steps": 7168,
-        "gamma": 0.916,
-        "learning_rate": 5.7e-5,
-        "clip_range": 0.39,
-        "gae_lambda": 0.98,
-    }
-    model = PPO(
-        "CnnPolicy",
-        env,
-        verbose=1,
-        device="mps",
-        tensorboard_log=LOG_DIR,
-        **model_params,
-    )
-    model.learn(total_timesteps=50_000, progress_bar=True)
-    model.save("./opt/test_50k")
+    # model_params = {
+    #     "n_steps": 7168,
+    #     "gamma": 0.916,
+    #     "learning_rate": 5.7e-5,
+    #     "clip_range": 0.39,
+    #     "gae_lambda": 0.98,
+    # }
+    # model = PPO(
+    #     "CnnPolicy",
+    #     env,
+    #     verbose=1,
+    #     device="mps",
+    #     tensorboard_log=LOG_DIR,
+    #     **model_params,
+    # )
+    model = PPO.load(model_path, env, device="mps", verbose=1, tensorboard_log=LOG_DIR, ent_coef=0.01)
+    model.learn(total_timesteps=timesteps, progress_bar=True, callback=callback)
+    model.save(save_path)
     env.close()
     print("done")
 
---
+++ testing.py
@@ -0,0 +1,52 @@
+import imageio.v2 as imageio
+import numpy as np
+from model import StreetFighter
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv, VecFrameStack
+class StreetFighterWatch(StreetFighter):
+    def step(self, action):
+        obs, reward, terminated, truncated, info = self.game.step(action)
+        self.last_rgb = np.asarray(obs).copy()
+        obs = self.preprocess(obs)
+        self.previous_frame = obs
+        if info["health"] == 0 and info["enemy_health"] == 0:
+            reward = 0
+            self.enemy_health = 0
+            self.player_health = 0
+        else:
+            dmg_dealt = self.enemy_health - info["enemy_health"]
+            dmg_taken = self.player_health - info["health"]
+            self.enemy_health = info["enemy_health"]
+            self.player_health = info["health"]
+            reward = dmg_dealt - dmg_taken
+        return obs, reward, terminated, truncated, info
+    def reset(self, seed=None, options=None):
+        super(StreetFighter, self).reset(seed=seed)
+        obs, info = self.game.reset(seed=seed, options=options)
+        self.last_rgb = np.asarray(obs).copy()
+        obs = self.preprocess(obs)
+        self.previous_frame = obs
+        info = self.game.data.lookup_all()
+        self.player_health = info.get("health", 0)
+        self.enemy_health = info.get("enemy_health", 0)
+        return obs, info
+
+model_path = "./opt/sf_5m.zip"
+
+env = DummyVecEnv([StreetFighterWatch])
+env = VecFrameStack(env, n_stack=4, channels_order="last")
+model = PPO.load(model_path, env=env)
+
+speed = 4
+
+obs = env.reset()
+frames = []
+while True:
+    action, _ = model.predict(obs, deterministic=False)
+    obs, rewards, dones, infos = env.step(action)
+    frames.append(env.venv.envs[0].last_rgb)
+    if dones[0]:
+        break
+env.close()
+imageio.mimsave("sf_5m_{}x.mp4".format(speed), frames, fps=30 * speed)
+print("saved mp4")
